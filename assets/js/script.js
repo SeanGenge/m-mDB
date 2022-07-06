d6c409b23f8cc6e05ebd18137e72279920a29b51
Ishan <ishanwijes@gmail.com>
2022-07-06 19:46:07 +1000

    added albums instead of individual songs, ignore last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,13 @@ let favouritesArrayURLS = [];
 
 
 
+
+
+
+
+
+// most popular movies section
+
 // creating a function for fetching the "popular" category of movie data
 const fetchPopularMovies = function() {
     fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=` + apiKey)
@@ -80,41 +87,72 @@ let movieSwiper = new Swiper(".mySwiper", {
 
 
 
-let popularSongsArray20 = [];
 
+
+
+
+
+// most popular songs section
+
+let popularAlbumsArray20 = [];
+let starSongElements;
 
 
 // fetching song data from audioDB
-fetch('https://theaudiodb.com/api/v1/json/523532/mostloved.php?format=track')
+fetch('https://theaudiodb.com/api/v1/json/523532/mostloved.php?format=album')
 .then(responce => responce.json())
 .then(data => {
 
     console.log(data);
 
     data.loved.forEach(function(object, i) {
-        // extract the first 20 popular songs, no need for all 50
-        if(i <= 23) {
-            popularSongsArray20.push(object);
+        // extract the first 20 popular albums, no need for all 50
+        if(i <= 19) {
+            popularAlbumsArray20.push(object);
         }
 
     })
 
-    popularSongsArray20.forEach(function(object, i) {
-
-        if(object.strTrackThumb !== null) {
+    popularAlbumsArray20.forEach(function(object, i) {
 
             swiperWrapperSongs.insertAdjacentHTML('beforeend', 
-                `<div class="swiper-slide" data-objectid="${object.idTrack}">
-                    <img class="star" src="./assets/images/star.png"/>
-                    <p class="song-name">${object.strTrack}</p>
-                    <img class="poster" src="${object.strTrackThumb}"/>
+                `<div class="swiper-slide" data-objectid="${object.idAlbum}">
+                    <img class="star-song" src="./assets/images/star.png"/>
+                    <p class="song-name">${object.strAlbum}</p>
+                    <img class="poster" src="${object.strAlbumThumb}"/>
                     <p class="rating-circle">${Number(object.intScore).toFixed(1)}</p>
                 </div>`
             )
 
-        }
 
     })
+
+
+
+
+
+    
+    // add event listener to all star elements for SONGS
+    starSongElements = document.querySelectorAll('.star-song');
+    
+    starSongElements.forEach(function(element) {
+
+        element.addEventListener('click', function() {
+            element.classList.toggle('fav-song');
+            console.log('clicked!');
+
+            let currentSongCard = element.closest('.swiper-slide');
+
+            if(element.classList.contains('fav-song')) {
+                // window.localStorage.setItem(`song-${currentSongCard}`)
+            }
+
+        })
+
+    })
+
+
+
 
 })
 
